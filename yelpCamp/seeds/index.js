6a94b80a79b25335b64e0b3143c650cb0f98770a
Shivam Yadav <shivam17106068@gmail.com>
2021-05-21 10:59:59 +0530

    layouts and partials with basic bootstrap added

--- yelpCamp/seeds/index.js
+++ yelpCamp/seeds/index.js
@@ -21,9 +21,13 @@ const seeddb = async ()=>{
     await campGround.deleteMany({});
     for(let i=0; i<50;i++){
         const random1000= Math.floor(Math.random()*1000);
+        const price= Math.floor(Math.random()*30)+10;
         const camp= new campGround({
             location: `${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            discription: 'discription text here',
+            price
         })
         await camp.save();
     }
