9a059586a4f2a6e75d54a26aa7ade35a4b23934e
Shivam Yadav <shivam17106068@gmail.com>
2021-05-29 08:08:29 +0530

    signed cookies done

--- cookesDemo/index.js
+++ cookesDemo/index.js
@@ -3,13 +3,21 @@ const app= express();
 
 const cookieParser= require('cookie-parser');
 
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet',(req,res)=>{
     const {name='shivem'}=req.cookies;
     res.send(`hello greeting for the day ${name}`);
 })
 
+app.get('/signedcookies',(req,res)=>{
+    res.cookie('fruit','grape',{signed: true});
+    res.send('added signed cookies');
+})
+app.get('/verifyfruit',(req,res)=>{
+    console.log(req.signedCookies);
+    res.send(req.signedCookies)
+})
 app.get('/setname',(req,res)=>{
     res.cookie('name','henrietta')
     res.cookie('animal','harlequin shrimp')
